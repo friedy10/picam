cf078354f3121e29fdd9c100efc156918f185dc2
rabbitcat59 <72715674+rabbitcat59@users.noreply.github.com>
2020-10-13 21:56:07 -0400

    yeet5

--- main.py
+++ main.py
@@ -100,9 +100,9 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='1920x1080', framerate=30) as camera:
+with picamera.PiCamera(resolution='640x480', framerate=30) as camera:
     output = StreamingOutput()
-    camera.brightness = 100
+    camera.brightness = 55
     #Uncomment the next line to change your Pi's Camera rotation (in degrees)
     #camera.rotation = 90
     camera.start_recording(output, format='mjpeg')
