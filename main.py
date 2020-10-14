c598f2500ff6ecb1a5cfc3749dde9730a7cf3bc3
FarmerLarry <bjchapler@gmail.com>
2020-10-13 21:54:03 -0400

    danunununu

--- main.py
+++ main.py
@@ -15,11 +15,30 @@ PAGE="""\
 </head>
 <body>
 <center style="color:white"><h1>Fun Friday Security Cam</h1></center>
-<center><img src="stream.mjpg" width="1920" height="1080"></center>
+<center><img src="stream.mjpg" width="640" height="480"></center>
 </body>
 </html>
 
+<div data-video="toAdCKXu75E"
+         data-autoplay="1"
+         data-loop="1"
+         id="youtube-audio">
+  </div>
+  <script src="https://www.youtube.com/iframe_api"></script>
+  <script src="https://cdn.rawgit.com/labnol/files/master/yt.js"></script>
+
+<center style="color:white"><h1>Mute Above This</h1></center>
+
+function enableMute() { 
+  data-video.muted = true;
+} 
+
+function disableMute() { 
+  data-video = false;
+} 
+
 """
+
 #comment boiiiiiiiiiii
 class StreamingOutput(object):
     def __init__(self):
@@ -83,7 +102,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 with picamera.PiCamera(resolution='1920x1080', framerate=30) as camera:
     output = StreamingOutput()
-    camera.brightness = 55
+    camera.brightness = 100
     #Uncomment the next line to change your Pi's Camera rotation (in degrees)
     #camera.rotation = 90
     camera.start_recording(output, format='mjpeg')
